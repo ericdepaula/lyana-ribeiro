ad570cc354d44b711e01dacf164e2e0a270339ba
Eric de Paula <ericdepaula39@gmail.com>
2025-10-13 12:23:25 -0300

    refactor: adiciona imagem como fallback

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,9 +20,11 @@ const Hero = () => {
 
       <video
         ref={videoRef}
+        autoPlay
         loop
         muted
         playsInline
+        poster="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         className="absolute inset-0 w-full h-full object-cover"
       >
         <source
