6c8cab7defe2781e4ccb4d70faec6e19944f5f27
Eric de Paula <ericdepaula39@gmail.com>
2025-10-16 13:35:46 -0300

    reafctor: adiciona lazy load para melhora de carregamento em dispositivos mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import CTA from './components/CTA';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { lazy, Suspense } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const About = lazy(() => import("./components/About"));
+const Services = lazy(() => import("./components/Services"));
+const CTA = lazy(() => import("./components/CTA"));
+const Testimonials = lazy(() => import("./components/Testimonials"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
   return (
     <div className="scroll-smooth">
       <Navbar />
       <Hero />
-      <About />
-      <Services />
-      <CTA />
-      <Testimonials />
-      <Contact />
-      <Footer />
+      <Suspense fallback={null}>
+        {" "}
+        <About />
+        <Services />
+        <CTA />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
